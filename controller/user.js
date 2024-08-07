69e940482515a49481f507169827a33c8c868026
arex123 <ad47kumar@gmail.com>
2024-08-07 12:39:31 +0530

    login backend

--- controller/user.js
+++ controller/user.js
@@ -2,7 +2,35 @@ const User = require('../models/Users')
 const path = require('path')
 
 exports.showLogin = (req,res,next)=>{
-    res.sendFile()
+    res.sendFile(path.join(__dirname,'../views','signup','signin.html'))
+}
+
+exports.submitLogin = (req,res,next)=>{
+
+    let login = req.body
+    console.log("login info ",login)
+    User.findAll({where:{email:login.email}}).then(user=>{
+        if(user.length==0){
+            res.status(404).json({
+                success:false,
+                message:"User not found"
+            })
+        }else{
+            user = user[0]
+            console.log("user from db ",user.password)
+            if(user.password!=login.password){
+                res.status(401).json({
+                    success:false,
+                    message:"User Not Authorized"
+                })
+            }else{
+                res.status(200).json({
+                    success:true,
+                    message:"User is succesfuly logged in"
+                })
+            }
+        }
+    })
 }
 
 
