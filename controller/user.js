7d88bad186f8ea48547893b3a1727ec83a921301
arex123 <ad47kumar@gmail.com>
2024-08-07 13:37:06 +0530

    encrypted psd, using bcrypt

--- controller/user.js
+++ controller/user.js
@@ -1,6 +1,6 @@
 const User = require('../models/Users')
 const path = require('path')
-
+const bcrypt = require('bcrypt')
 exports.showLogin = (req,res,next)=>{
     res.sendFile(path.join(__dirname,'../views','signup','signin.html'))
 }
@@ -18,17 +18,24 @@ exports.submitLogin = (req,res,next)=>{
         }else{
             user = user[0]
             console.log("user from db ",user.password)
-            if(user.password!=login.password){
-                res.status(401).json({
-                    success:false,
-                    message:"User Not Authorized"
-                })
-            }else{
-                res.status(200).json({
-                    success:true,
-                    message:"User is succesfuly logged in"
-                })
-            }
+
+            //comparing password with has in db
+            bcrypt.compare(login.password,user.password).then(result=>{
+                console.log("rsult ",result)
+                if(result==false){
+                    res.status(401).json({
+                        success:false,
+                        message:"User Not Authorized"
+                    })
+                }else{
+                    res.status(200).json({
+                        success:true,
+                        message:"User is succesfuly logged in"
+                    })
+                }
+            })
+
+
         }
     })
 }
@@ -40,16 +47,19 @@ exports.showSignup = (req,res)=>{
 
 exports.createUser=(req,res)=>{
     console.log(req.body)
-
-    User.create({name:req.body.name,email:req.body.email,password:req.body.password})
-    .then(user=>{
-        res.json(user)
-    }).catch(error=>{
-        let errormsg = error?.errors?.[0]?.message ?? error.message
-
-        res.json({
-            error:errormsg
+    //encrypting our password
+    let plainPsd = req.body.password
+    let salt = 10
+    bcrypt.hash(plainPsd,salt).then(hash=>{
+        console.log("hash ",hash)
+        User.create({name:req.body.name,email:req.body.email,password:hash})
+        .then(user=>{
+            res.json(user)
+        }).catch(error=>{
+            let errormsg = error?.errors?.[0]?.message ?? error.message    
+            res.json({
+                error:errormsg
+            })
         })
     })
-
 }
